48d2e294a6030f46ab2d02bd3083aa6dde8a8d7d
Gucioo <tomasz.baryla@gmail.com>
2025-09-23 23:34:39 +0100

    Update __init__.py
    Enhanced __init__.py with timeout configuration

--- components/modbus_tcp_manager/__init__.py
+++ components/modbus_tcp_manager/__init__.py
@@ -9,6 +9,8 @@ CONF_UNIT_ID = "unit_id"
 CONF_WATCHDOG_REGISTER = "watchdog_register"
 CONF_WATCHDOG_INTERVAL = "watchdog_interval"
 CONF_SAFE_MODE_REGISTERS = "safe_mode_registers"
+CONF_CONNECTION_TIMEOUT = "connection_timeout"
+CONF_READ_TIMEOUT = "read_timeout"
 
 # Namespace
 modbus_tcp_ns = cg.esphome_ns.namespace("modbus_tcp")
@@ -33,6 +35,9 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_WATCHDOG_REGISTER): cv.positive_int,
     cv.Optional(CONF_WATCHDOG_INTERVAL, default="10s"): cv.positive_time_period_milliseconds,
     cv.Optional(CONF_SAFE_MODE_REGISTERS, default=[]): cv.All(cv.ensure_list(SAFE_MODE_REGISTER_SCHEMA)),
+    # New timeout configurations for slow devices
+    cv.Optional(CONF_CONNECTION_TIMEOUT, default="5s"): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_READ_TIMEOUT, default="5s"): cv.positive_time_period_milliseconds,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -51,5 +56,9 @@ async def to_code(config):
     # Add safe mode registers if specified
     for safe_reg in config[CONF_SAFE_MODE_REGISTERS]:
         cg.add(var.add_safe_mode_register(safe_reg["register"], safe_reg["value"]))
+    
+    # Add timeout configurations
+    cg.add(var.set_connection_timeout(config[CONF_CONNECTION_TIMEOUT]))
+    cg.add(var.set_read_timeout(config[CONF_READ_TIMEOUT]))
     
     await cg.register_component(var, config)
